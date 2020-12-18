2cc45b0a6ab536139c6eb026a058f4c7cee2c668
wukeke <752869309@qq.com>
2020-12-18 19:06:19 +0800

    feat:create a gin.log for logging.

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"Backend/application/route"
 	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"os"
+	"syscall"
 )
 
 func main() {
@@ -12,6 +16,15 @@ func main() {
 
 	// 不使用中间件
 	ginObject := gin.New()
+
+	gin.ForceConsoleColor()
+	// 修改日志的默认位置
+	file, err := os.OpenFile("gin.log", syscall.O_RDWR, 0666)
+	if err != nil {
+		log.Println("fail to open gin.log.")
+		return
+	}
+	gin.DefaultWriter = io.MultiWriter(file, os.Stdout)
 	ginObject.Use(gin.Logger())
 	route.LoadRouteConfig(ginObject)
 	_ = ginObject.Run(":8080")
